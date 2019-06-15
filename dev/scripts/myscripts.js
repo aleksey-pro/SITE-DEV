6cf613c2eb2f3697d4d91743ff5904cb879f9dfb
davegahn <anxieter@gmail.com>
2019-06-16 00:37:00 +0300

    New texts and fixes

--- dev/scripts/myscripts.js
+++ dev/scripts/myscripts.js
@@ -3,24 +3,24 @@ $(document).ready(function() {
 
   //active link on click
 
-  $(".nav__item").on("click", function() {
-    var list = $(this).closest("ul");
-    list.find(".nav__item").removeClass("active");
-    $(this).toggleClass("active");
+  $('.nav__item').on('click', function() {
+    var list = $(this).closest('ul');
+    list.find('.nav__item').removeClass('active');
+    $(this).toggleClass('active');
   });
 
-  $(".title1").css("display", "block");
+  $('.title1').css('display', 'block');
 
   // works horizontal scrolling
 
   var myScroll;
 
   function adjustCardsContainer() {
-    var workCardWidth = $(".grid__item").width();
-    var workCardQnty = $(".grid__item").length;
+    var workCardWidth = $('.grid__item').width();
+    var workCardQnty = $('.grid__item').length;
     // var sw = (window.matchMedia("(max-width: 460px)").matches) ? workCardWidth * workCardQnty + 100 : (workCardWidth * (workCardQnty / 2) + 100);
     var sw = workCardWidth * Math.ceil(workCardQnty / 2) + 100;
-    $("#scroller").css("width", sw + "px");
+    $('#scroller').css('width', sw + 'px');
   }
 
   adjustCardsContainer();
@@ -32,7 +32,7 @@ $(document).ready(function() {
   //оризонтальный скроллл секции работ
 
   function horScroll() {
-    myScroll = new IScroll("#scrolling-wrapper", {
+    myScroll = new IScroll('#scrolling-wrapper', {
       eventPassthrough: true,
       scrollX: true,
       scrollY: false,
@@ -40,7 +40,7 @@ $(document).ready(function() {
       interactiveScrollbars: true,
       mouseWheel: false,
       keyBindings: true,
-      useTransition: false
+      useTransition: false,
     });
   }
 
@@ -52,14 +52,14 @@ $(document).ready(function() {
 
   // fullpage settings
 
-  $("#fullpage").fullpage({
-    anchors: ["advantages", "services", "works", "start", "order"],
-    menu: "#menu",
+  $('#fullpage').fullpage({
+    anchors: ['advantages', 'services', 'works', 'start', 'order'],
+    menu: '#menu',
     scrollBar: false,
     controlArrows: false,
-    slidesNavigation: true,
-    slidesNavPosition: "bottom",
-    paddingTop: "50px",
+    slidesNavigation: false,
+    slidesNavPosition: 'bottom',
+    paddingTop: '50px',
     // Докрутка
     // fitToSection: false,
     // scrollOverflow:true,
@@ -77,11 +77,13 @@ $(document).ready(function() {
 
       //настройка слайдера
 
-      function fade(slide) {
+      function fade(title) {
         setTimeout(function() {
-          $(".slide__title").css("display", "none");
-          $(".slide__subtitle").css("display", "none");
-          $(slide).fadeIn("4000");
+          $('.slide__title').css('display', 'none');
+          $(title).each(function(idx, title) {
+            var i = idx + 2;
+            $(title).fadeIn(idx * '2000');
+          });
         }, 1000);
       }
 
@@ -98,103 +100,95 @@ $(document).ready(function() {
       //   // break;
       // }
 
-      if (index == 1 && slideIndex == 0 && direction == "right") {
-        fade(".title2");
+      if (index == 1 && slideIndex == 0 && direction == 'right') {
+        fade('.title2');
       }
 
-      if (index == 1 && slideIndex == 1 && direction == "right") {
-        fade(".title3");
+      if (index == 1 && slideIndex == 1 && direction == 'right') {
+        fade('.title3');
       }
 
-      if (index == 1 && slideIndex == 2 && direction == "right") {
-        fade(".title4");
+      if (index == 1 && slideIndex == 2 && direction == 'left') {
+        fade('.title1');
       }
-
-      if (index == 1 && slideIndex == 3 && direction == "right") {
-        fade(".title5");
-      }
-
-      if (index == 1 && slideIndex == 4 && direction == "left") {
-        fade(".title1");
-      }
-    }
+    },
   }); // fullpage init
 
   setInterval(function() {
     $.fn.fullpage.moveSlideRight();
-  }, 5000);
+  }, 7000);
 
   //nice-select for inputs
 
-  $(".order-form select").niceSelect();
+  $('.order-form select').niceSelect();
 
   //toggle hamburger menu
 
-  $(".hamburger").on("click", function() {
-    $(this).toggleClass("is-active");
-    $(".nav-dropdown").slideToggle("slow");
+  $('.hamburger').on('click', function() {
+    $(this).toggleClass('is-active');
+    $('.nav-dropdown').slideToggle('slow');
   });
 
-  $(".nav__link").on("click", function() {
-    $(".nav-dropdown").slideUp("fast");
-    $(".hamburger").removeClass("is-active");
+  $('.nav__link').on('click', function() {
+    $('.nav-dropdown').slideUp('fast');
+    $('.hamburger').removeClass('is-active');
   });
 
   //price-tabs
 
-  var $wrapper = $(".price-table"),
-    $allTabs = $wrapper.find(".price-table__block"),
-    $tabMenu = $wrapper.find(".price-table__item");
+  var $wrapper = $('.price-table'),
+    $allTabs = $wrapper.find('.price-table__block'),
+    $tabMenu = $wrapper.find('.price-table__item');
 
-  $allTabs.not(":first-of-type").hide();
+  $allTabs.not(':first-of-type').hide();
 
   $tabMenu.each(function(i) {
-    $(this).attr("data-tab", "tab" + i);
+    $(this).attr('data-tab', 'tab' + i);
   });
   $allTabs.each(function(i) {
-    $(this).attr("data-tab", "tab" + i);
+    $(this).attr('data-tab', 'tab' + i);
   });
 
-  $tabMenu.on("click", function() {
-    var dataTab = $(this).data("tab");
+  $tabMenu.on('click', function() {
+    var dataTab = $(this).data('tab');
     $getWrapper = $(this).closest($wrapper);
-    $getWrapper.find($tabMenu).removeClass("active");
-    $(this).removeClass("hovered");
-    $(this).addClass("active");
+    $getWrapper.find($tabMenu).removeClass('active');
+    $(this).removeClass('hovered');
+    $(this).addClass('active');
     $getWrapper.find($allTabs).hide();
     $getWrapper
       .find($allTabs)
-      .filter("[data-tab=" + dataTab + "]")
+      .filter('[data-tab=' + dataTab + ']')
       .show();
   });
 
-  $tabMenu.on("mouseenter mouseleave", function() {
-    if (!$(this).hasClass("active")) {
-      $(this).toggleClass("hovered");
+  $tabMenu.on('mouseenter mouseleave', function() {
+    if (!$(this).hasClass('active')) {
+      $(this).toggleClass('hovered');
     }
   });
 
   // mailsend
 
-  $("#form").on("submit", function(e) {
+  $('#form').on('submit', function(e) {
     e.preventDefault();
     var fd = new FormData(this);
     $.ajax({
-      url: "../send.php",
-      type: "POST",
+      url: '../send.php',
+      type: 'POST',
       contentType: false,
       processData: false,
       data: fd,
       success: function(msg) {
-        if (msg == "ok") {
-          $(".info").text("Отправлено");
-          $("input, textarea, select")
-            .not(":input[type=file], :input[type=submit]")
-            .val("");
+        if (msg == 'ok') {
+          $('.info').text('Отправлено');
+          $('input, textarea, select')
+            .not(':input[type=file], :input[type=submit]')
+            .val('');
         } else {
-          $(".info").text("Ошибка");
+          $('.info').text('Ошибка');
         }
-      }
+      },
     });
   });
 }); //ready
